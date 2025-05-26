ac58b6eb11ecc0ea5edd2775c5ed228b59a8abae
Husnain <husnainali41940@gmail.com>
2025-05-26 01:29:18 -0700

    Implemented Auth, Billing, Database Setup, Create Companion Server action.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,53 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import {recentSessions} from "@/constants";
+import Cta from '@/components/CTA'
 import React from 'react'
 
 const Page = () => {
+    //   const companions = await getAllCompanions({ limit: 3 });
+    // const recentSessionsCompanions = await getRecentSessions(10);
   return (
-    <div>
-<h1 className='text-2xl underline'>
-
+    <main>
+<h1 >
+Popular Companions
 </h1>
+<section className='home-section'>
+<CompanionCard
+id="123"
+name="Neura the Brainy Explorer"
+topic="Neural Network of the Brain"
+subject="science"
+duration={45}
+color="#ffda6e"
+/>
+<CompanionCard
+id="456"
+name="Countsy the Number Wizard"
+topic="Derivatives and Integrals"
+subject="Maths"
+duration={30}
+color="#e5d0ff"
+/>
+<CompanionCard
+id="789"
+name="Verba the Vocabulary Builder"
+topic="language"
+subject="English Literature"
+duration={30}
+color="#BDE7FF"
+/>
+</section>
+<section className= 'home-section'>
+ <CompanionsList
+                title="Recently completed sessions"
+                companions={recentSessions}
+                classNames="w-2/3 max-lg:w-full"
+            />
+<Cta />
+</section>
 
-    </div>
+    </main>
   )
 }
 
