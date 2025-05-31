655b935c79765fd8b935f51e259d4820d05d5d77
Husnain <husnainali41940@gmail.com>
2025-05-31 00:48:46 -0700

    Remove Errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
-import { Bricolage_Grotesque } from "next/font/google";
+// import { Bricolage_Grotesque } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
-const bricolage = Bricolage_Grotesque({
-  variable: "--font-bricolage",
-  subsets: ["latin"],
-});
+// const bricolage = Bricolage_Grotesque({
+//   variable: "--font-bricolage",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Converso",
@@ -21,7 +21,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>
+     <body className="font-bricolage antialiased">
+
         <ClerkProvider appearance={{ variables: { colorPrimary: '#fe5933' }} }>
           <Navbar />
           {children}
